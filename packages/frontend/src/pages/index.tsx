71f79eaa4ea84c3850cd1ca17b2bd1b84a0fd127
drhongos <lucasziegenfuhs@gmail.com>
2023-06-05 01:30:56 +0200

    frontend added libp2p attrs

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,19 +1,91 @@
 import Head from 'next/head'
-import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/20/solid'
-import Nav from '@/components/nav'
+//import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/20/solid'
+//import Nav from '@/components/nav'
 import { useLibp2pContext } from '@/context/ctx'
 import type { Connection } from '@libp2p/interface-connection'
 import { usePeerContext } from '../context/peer-ctx'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState, useRef } from 'react'
 import Image from 'next/image'
 import { multiaddr } from '@multiformats/multiaddr'
 import { connectToMultiaddr } from '../lib/libp2p'
+import ChatContainer from '@/components/chat'
+import { createIcon } from '@download/blockies'
 
+type PeerProtoTuple = {
+  peerId: string
+  protocols: string[]
+}
+
+function short_text(text: string): string {
+  return `${text.slice(0, 5)}...${text.slice(-5)}`
+}
+
+function copyToClipboard(text: string) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      console.log('Text copied to clipboard');
+    })
+    .catch((error) => {
+      console.error('Error copying text to clipboard:', error);
+    });
+}
+
+function PeerData({peerId, protocols}: PeerProtoTuple) {
+  const msgref = useRef<HTMLLIElement>(null)
+  useEffect(() => {
+    const icon = createIcon({
+      seed: peerId,
+      size: 15,
+      scale: 3,
+    })
+    icon.className = 'rounded mr-2 max-h-8 max-w-8'
+    const childrenCount = msgref.current?.childElementCount
+    // Prevent inserting an icon more than once.
+    if (childrenCount && childrenCount < 2) {
+      msgref.current?.insertBefore(icon, msgref.current?.firstChild)
+    }
+  }, [peerId])
+
+  return (
+    <li ref={msgref} className={`flex justify-start`}>
+      <div
+        className="flex relative max-w-xl px-4 py-2 text-gray-700 rounded shadow bg-white"
+        style={{justifyContent: 'space-between', width: '100%'}}
+      >
+        {short_text(peerId)}
+        <button
+          style={{
+            border: '1px solid gray',
+            borderRadius: '5px',
+            backgroundColor: 'lightgray',
+            padding: '5px'
+          }} 
+          onClick={() => copyToClipboard(`${peerId}`)}
+        >Copy</button>
+      </div>
+    </li>
+  )
+}
 
 export default function Home() {
   const { libp2p } = useLibp2pContext()
   const { peerStats, setPeerStats } = usePeerContext()
   const [maddr, setMultiaddr] = useState('')
+
+  const msgref = useRef<HTMLLIElement>(null)
+  useEffect(() => {
+    const icon = createIcon({
+      seed: libp2p.peerId,
+      size: 15,
+      scale: 3,
+    })
+    icon.className = 'rounded mr-2 max-h-10 max-w-10'
+    const childrenCount = msgref.current?.childElementCount
+    // Prevent inserting an icon more than once.
+    if (childrenCount && childrenCount < 2) {
+      msgref.current?.insertBefore(icon, msgref.current?.firstChild)
+    }
+  }, [libp2p])
 
   useEffect(() => {
     const peerConnectedCB = (evt: any) => {
@@ -27,11 +99,6 @@ export default function Home() {
       libp2p.removeEventListener('peer:connect', peerConnectedCB)
     }
   }, [libp2p, peerStats, setPeerStats])
-
-  type PeerProtoTuple = {
-    peerId: string
-    protocols: string[]
-  }
 
   const getFormattedConnections = (connections: Connection[]): PeerProtoTuple[] => {
     const protoNames: Map<string, string[]> = new Map()
@@ -75,8 +142,6 @@ export default function Home() {
     [libp2p, maddr],
   )
 
-  // handleConnectToMultiaddr
-
   const handleMultiaddrChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       setMultiaddr(e.target.value)
@@ -93,7 +158,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="min-h-full">
-        <Nav />
+{/*         <Nav /> */}
         <div className="py-10">
           <header>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -109,64 +174,81 @@ export default function Home() {
             </div>
           </header>
           <main>
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <ul className="my-2 space-y-2 break-all">
-                <li className="">This PeerID: {libp2p.peerId.toString()}</li>
-              </ul>
-              <div className="my-6 w-1/2">
-                <label
-                  htmlFor="peer-id"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  multiaddr to connect to
-                </label>
-                <div className="mt-2">
-                  <input
-                    value={maddr}
-                    type="text"
-                    name="peer-id"
-                    id="peer-id"
-                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="12D3Koo..."
-                    aria-describedby="multiaddr-id-description"
-                    onChange={handleMultiaddrChange}
-                  />
+            <div style={{display: "flex"}}>
+              <div style={{width: '30%'}}>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <h3 className="text-xl">
+                  {' '}
+                  Your node
+                </h3>
+                <PeerData
+                  peerId={libp2p.peerId.toString()}
+                  protocols={[]}
+                />
+                <h3 className="text-xl">
+                  {' '}
+                  Your topics
+                </h3>
+                <ul>
+                  {libp2p.pubsub.getTopics().map((d) => 
+                    <li>{d}</li>
+                  )}
+                </ul>
+{/* make it an expandable tool
+                <div className="my-6 w-1/2">
+                  <label
+                    htmlFor="peer-id"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    multiaddr to connect to
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      value={maddr}
+                      type="text"
+                      name="peer-id"
+                      id="peer-id"
+                      className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      placeholder="12D3Koo..."
+                      aria-describedby="multiaddr-id-description"
+                      onChange={handleMultiaddrChange}
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className="rounded-md bg-indigo-600 my-2 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={handleConnectToMultiaddr}
+                  >
+                    Connect to multiaddr
+                  </button>
                 </div>
-                <button
-                  type="button"
-                  className="rounded-md bg-indigo-600 my-2 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  onClick={handleConnectToMultiaddr}
-                >
-                  Connect to multiaddr
-                </button>
+ */}
+                <div>
+                  {peerStats.peerIds.length > 0 ? (
+                    <>
+                      <h3 className="text-xl">
+                        {' '}
+                        Connected peers ({getFormattedConnections(peerStats.connections).length}){' '}
+                      </h3>
+                      <pre className="px-2">
+                        {getFormattedConnections(peerStats.connections)
+                          .map(
+                            (pair) =>
+                              <PeerData
+                                key={pair.peerId}
+                                peerId={pair.peerId}
+                                protocols={pair.protocols}
+                              />                            
+                          )}
+                      </pre>
+                    </>
+                  ) : null}
+                </div>
               </div>
 
-
-              <div className="my-4 inline-flex items-center text-xl">
-                Connected:{' '}
-                {peerStats.connected ? (
-                  <CheckCircleIcon className="inline w-6 h-6 text-green-500" />
-                ) : (
-                  <XCircleIcon className="w-6 h-6 text-red-500" />
-                )}
               </div>
-              <div>
-                {peerStats.peerIds.length > 0 ? (
-                  <>
-                    <h3 className="text-xl">
-                      {' '}
-                      Connected peers ({getFormattedConnections(peerStats.connections).length}) 👇
-                    </h3>
-                    <pre className="px-2">
-                      {getFormattedConnections(peerStats.connections)
-                        .map(
-                          (pair) =>
-                            `${pair.peerId} (${pair.protocols.join(', ')})`,
-                        )
-                        .join('\n')}
-                    </pre>
-                  </>
-                ) : null}
+              <div style={{width: '70%'}}>
+                <ChatContainer />
               </div>
             </div>
           </main>
@@ -175,3 +257,4 @@ export default function Home() {
     </>
   )
 }
+
